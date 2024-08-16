bc129a43c3d3d2c855b5cd7c204baf20ceb9b458
Triffycodes <adarsh.s.gowda20@gmail.com>
2024-08-16 12:40:21 -0500

    recoil with hardcoded recoil states and selcters

--- week-7-recoil/src/App.jsx
+++ week-7-recoil/src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { jobsAtom, messagingAtom, networkAtom, notificationsAtom } from './atom';
+import { RecoilRoot, useRecoilValue } from 'recoil';
 
 function App() {
-  const [count, setCount] = useState(0)
+  return <RecoilRoot>
+    <MainApp/>
+  </RecoilRoot>
+}
 
+function MainApp(){
+  const networkCount = useRecoilValue(networkAtom);
+  const messageCount = useRecoilValue(messagingAtom);
+  const jobsCount = useRecoilValue(jobsAtom);
+  const notificationCount = useRecoilValue(notificationsAtom);
   return <div>
-      hi
+      <button>Home</button>
+
+      <button>Network ({networkCount >= 100 ? "99+" : networkCount})</button>
+      <button>Messages ({messageCount >= 100 ? "99+" : messageCount})</button>
+      <button>Jobs ({jobsCount >= 100 ? "99+" : jobsCount})</button>
+      <button>Notifications ({notificationCount >= 100 ? "99+" : notificationCount})</button>
+
+      <button>Me</button>
    </div>
 }
 
